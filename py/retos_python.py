54ddb61f206739ff65ed6d7674477abaaf0f5d90
Johyner Steven Martinez Cepeda <johynersmc1029@gmail.com>
2026-09-03 20:56:03 -0500

    Delete py directory

--- py/retos_python.py
+++
@@ -1,7 +0,0 @@
-## RETO DEL AÑO BISIESTO
-año=int(input("Ingrese el año que desea verificar si es bisiesto o no: "))
-if (año % 4 == 0 and año % 100 != 0) or (año % 400 == 0):
-    print(f"El año {año} es bisiesto")
-else:
-    print(f"El año {año} no es bisiesto")
-    
